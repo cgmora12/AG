d4bdb34b93c2b86a8438dd2fe863a09b7b7bd90a
cgmora12 <cgmora12@hotmail.com>
2018-03-14 13:33:56 +0100

    Automatic API generation tested with a csv file

---
+++ servercode.js
@@ -0,0 +1,73 @@
+'use strict';
+var toJson = require('allto-json');
+var fs = require('fs');
+
+exports.getListadodecentrosdelaUA = function(args, res, next) {
+
+  var examples = {};
+  var fileName = "./Listado de centros de la UA.csv";
+
+  if (fs.existsSync(fileName)) {
+    console.log('file exists');
+    // From csv to json
+    toJson.csv(fileName, function(error, result){
+      try{
+        // Creating json object
+        result = "{ \"csv\": " + JSON.stringify(result) + " }";
+        var jsonObj = JSON.parse(result);
+        var jsonResult = new Object();
+        jsonResult.csv = [];
+        console.log(jsonObj);
+        //console.log(JSON.stringify(args));
+
+        // Filtering results
+        var argsUndefined = true;
+        var filters = false;
+        var object_number = 0;
+
+        for(var i = 0; i < Object.keys(args).length; i++){
+          if(args[Object.keys(args)[i]].value != undefined){
+            argsUndefined = false;
+          }
+        }
+        if(argsUndefined){
+          jsonResult.csv = jsonObj.csv;
+        }
+        else {
+          for(var j = 0; j < Object.keys(args).length; j++){
+            if(args[Object.keys(args)[j]].value != undefined){
+              if(jsonResult.csv.length > 0 || filters){
+                jsonObj.csv = jsonResult.csv;
+                jsonResult.csv = [];
+                object_number = 0;
+              }
+              filters = true;
+              for(var i in jsonObj.csv){
+                if(jsonObj.csv[i][j] === args[Object.keys(args)[j]].value){
+                  jsonResult.csv[object_number] = jsonObj.csv[i];
+                  object_number++;
+                }
+              }
+            } 
+          }
+        }
+
+        console.log(jsonResult);
+        examples['application/json'] = jsonResult;
+      } catch (err) {
+          console.log(err);
+      }
+
+      if(Object.keys(examples).length > 0) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+      }
+      else {
+        res.end();
+      }
+    });
+  } 
+  else {
+        res.end();
+  }
+}
---
+++ src/AG/AG.java
@@ -0,0 +1,287 @@
+package AG;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class AG {
+
+	// Variables
+	public static String fileName = "Listado de centros de la UA";
+	public static String fileType = "csv";
+	public static String host = "virtserver.swaggerhub.com";
+	public static String basePath = "/cgmora12/lifts/1.0.0";
+	public static String swaggerFileName = "swaggerua.json";
+	public static String swaggerCodegeneFileName = "swagger-codegen-cli-2.2.1.jar";
+	public static String apiCodeFolderName = "apiCode";
+	public static String serverCodeFileName = "servercode.js";
+	
+	public static void main(String[] args) {
+		
+		//Automatic API Generation process
+		generateApiDefinition();
+        generateServer();
+        addServerDependencies();
+        generateApiCode();
+        
+        System.out.println("Automatic API Generation finished!");
+        
+	}
+
+	private static void generateApiDefinition() {
+		
+		String csvFile = fileName + "." + fileType;
+
+		String apiDefinition = "";
+		String fileNameNoWhiteSpaces = fileName.replaceAll(" ", "");
+		
+		BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        String[] columns = null;
+        String[] columnsDescriptions = null;
+        String[] exampleData = null;
+        String[] dataTypes = null;
+
+        try {
+        	// Read first rows of data file
+            br = new BufferedReader(new FileReader(csvFile));
+            if ((line = br.readLine()) != null) {
+                // use comma as separator
+            	//TODO: replace all rare characters
+                columnsDescriptions = line.replaceAll("\"", "").replaceAll("\'", "").split(cvsSplitBy);
+                columns = line.replaceAll(" ", "_").replaceAll("\"", "").replaceAll("\'", "").toLowerCase().split(cvsSplitBy);
+
+            }
+            if ((line = br.readLine()) != null) {
+            	//TODO: replace all rare characters
+                exampleData = line.replaceAll("\"", "").replaceAll("\'", "").split(cvsSplitBy);
+                dataTypes = new String[exampleData.length];
+                for(int i = 0; i < exampleData.length; i++) {
+                	try {
+                		if(exampleData[i].equals("" + Integer.parseInt(exampleData[i]))) {
+                    		dataTypes[i] = "integer";
+                		} else if(exampleData[i].equals("" + Float.parseFloat(exampleData[i]))){
+                    		dataTypes[i] = "number";
+                		} else {
+                    		dataTypes[i] = "string";
+                		}
+                	} catch (NumberFormatException e) {
+                		dataTypes[i] = "string";
+                	}
+                }
+
+            }
+            br.close();
+            
+            // Generate API definition and doc
+            apiDefinition += "{ \"swagger\" : \"2.0\", \"info\" : { \"version\" : \"1.0.0\", \"title\" : \"" + fileName + "\", \"description\" : \"Obtaining the " + fileNameNoWhiteSpaces  + "\" }, \"host\" : \"" + host + "\", \"basePath\" : \"" + basePath + "\",";
+            apiDefinition += "\"paths\" : { \"/\" : { \"get\" : { \"summary\" : \"GET " + fileName + "\", \"operationId\" : \"get" + fileNameNoWhiteSpaces + "\", \"produces\" : [ \"application/json\" ],";
+            apiDefinition += "\"parameters\" : [ ";
+            		
+            for(int i = 0; i < columns.length; i++) {
+            	if(i > 0) {
+            		apiDefinition += ",";
+            	}
+            	apiDefinition += "{ \"in\" : \"query\", \"name\" : \"" + columns[i] + "\", \"type\" : \"" + dataTypes[i] + "\", \"description\" : \"" + columnsDescriptions[i] + "\" }";
+                if(i == columns.length-1) {
+                	apiDefinition += " ],";
+                }
+            }
+            
+            apiDefinition += "\"responses\" : { \"200\" : { \"description\" : \"successful operation\", \"schema\" : { \"type\" : \"array\", \"items\" : { \"$ref\" : \"#/definitions/COLUMNS\" } } } } } } },";
+            
+            for(int i = 0; i < columns.length; i++) {
+            	if(i > 0) {
+            		apiDefinition += ",";
+        		} else {
+        			apiDefinition += "\"definitions\" : { \"COLUMNS\" : { \"type\" : \"object\", \"required\" : [ \"" + columns[i] + "\" ], \"properties\" : {";
+               	}
+            	apiDefinition += "\"" + columns[i] + "\" : { \"type\" : \"" + dataTypes[i] + "\", \"example\" : \"" + exampleData[i] + "\" }";
+                if(i == columns.length-1) {
+                	apiDefinition += "}, \"xml\" : { \"name\" : \"COLUMNS\" } } }, \"schemes\" : [ \"https\" ] }";
+                }
+            }
+            
+			// Create file
+            //new File(apiCodeFolderName).mkdirs();
+            PrintWriter writer = new PrintWriter(apiCodeFolderName + "/" + swaggerFileName, "UTF-8");
+            writer.println(apiDefinition);
+            writer.close();
+            
+            File source = new File(csvFile);
+            File dest = new File(apiCodeFolderName + "/" + csvFile);
+            try {
+                Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+		
+	}
+
+	private static void generateServer() {
+		Process proc = null;
+		try {
+			proc = Runtime.getRuntime().exec("java -jar " + apiCodeFolderName + "/" + swaggerCodegeneFileName 
+					+ " generate -i " + apiCodeFolderName + "/" + swaggerFileName + " -o " + apiCodeFolderName + " -l nodejs-server");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        try {
+			proc.waitFor();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+
+	private static void addServerDependencies() {
+		/*String npm = System.getProperty("os.name").toLowerCase().contains("win") ? "npm.cmd" : "npm";
+        Process proc2 = new ProcessBuilder("cmd", "npm install allto-json")
+                .directory(new File(apiCodeFolderName))
+                .start();*/
+        String lines = "";
+        BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(apiCodeFolderName + "/package.json"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        String line = "";
+        try {
+			while ((line = br.readLine()) != null) {
+				lines += line + "\n";
+				if(line.contains("\"dependencies\": {")) {
+					lines += "\t" + "\"allto-json\": \"^1.0.2\"," + "\n";
+				} 
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(apiCodeFolderName + "/package.json", "UTF-8");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        writer.println(lines);
+        writer.close();
+		
+	}
+
+
+	private static void generateApiCode() {
+
+		String servercode = "";
+		
+		BufferedReader br = null;
+		String line = "";
+        String lineFunctionName = "";
+        String lineFileName = "";
+		try {
+			br = new BufferedReader(new FileReader(apiCodeFolderName + "/controllers/DefaultService.js"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			while ((line = br.readLine()) != null) {
+				if(line.contains("function(args, res, next) {")) {
+					lineFunctionName = line + "\n";
+				}
+				else if(line.contains("var filename =")) {
+					lineFileName = line + "\n";
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        BufferedReader brServer = null;
+        String lineServer = "";
+		try {
+			brServer = new BufferedReader(new FileReader(serverCodeFileName));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			while ((lineServer = brServer.readLine()) != null) {
+				if(lineServer.contains("function(args, res, next) {")) {
+					servercode += lineFunctionName + "\n";
+				}
+				else if(lineServer.contains("var filename =")) {
+					servercode += lineFileName + "\n";
+				} 
+				else {
+					servercode += lineServer + "\n";
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+        	brServer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        
+        PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(apiCodeFolderName + "/controllers/DefaultService.js", "UTF-8");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        writer.println(servercode);
+        writer.close();
+		
+	}
+	
+}
