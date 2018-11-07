1e9f878cf67dbd2ab039e3dfbd72dd78687032f1
César González Mora <cesar_realmadrid_12@hotmail.com>
2018-11-07 13:03:26 +0100

    Removed unnecessary files

--- RunningExample/PruebaVisualizacion/ChartsDemo-macOS/Build-command-line/DerivedData/Build/Intermediates.noindex/Charts.build/Debug/Charts.build/Objects-normal/x86_64/Charts_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/cesargonzalezmora/eclipse-workspace/AG/RunningExample/PruebaVisualizacion/ChartsDemo-macOS/Build-command-line/DerivedData/Build/Intermediates.noindex/Charts.build/Debug/Charts.build/DerivedSources/Charts_vers.c
